3863931a698bc9262f98e2f6800b4add31b0724c
Yujun Qin <yujun_qin@brown.edu>
2016-10-22 17:04:26 -0400

    Add a few new functions

--- mine_sweep/src/main/resources/spark/template/freemarker/static/js/mine.js
+++ mine_sweep/src/main/resources/spark/template/freemarker/static/js/mine.js
@@ -1,10 +1,14 @@
+const ID_MAX = 999999;
+const ID_MIN = 100000;
+const SIZE = 31;
+const VAL_VISIBLE = 0;
+const STYLE_INVISIBLE = "#000000";
+const STYLE_SWEPT = "#e0e0e0";
+const STYLE_UNSWETPT = "#757575";
 var idUser;
 var idRoom;
 var webSocket;
 var grids;
-const ID_MAX = 999999;
-const ID_MIN = 100000;
-const SIZE = 31;
 
 function start() {
     idUser = Math.floor(Math.random() * (ID_MAX - ID_MIN + 1)) + ID_MIN;
@@ -19,10 +23,12 @@ function setupSocket() {
 }
 
 function joinRoom() {
+    // switch div
     document.getElementById("divOne").style.display = "none";
     document.getElementById("divTwo").style.display = "block";
-    setupSocket();
     idRoom = document.getElementById("room").value;
+    // connect to back end
+    setupSocket();
     webSocket.send({
         roomID: idRoom,
         userID: idUser
@@ -31,6 +37,7 @@ function joinRoom() {
 }
 
 function init() {
+    // instantiate array
     grids = new Array(SIZE);
     for (var i = 0; i < SIZE; i++) {
         grids[i] = new Array(SIZE);
@@ -40,6 +47,28 @@ function init() {
                 y: j,
                 mine: false,
                 visible: false
+            }
+        }
+    }
+}
+
+function isInRange(x, y, SIZE) {
+    return (x >= 0 && x < SIZE && y >= 0 && y < SIZE);
+}
+
+function changeState(x, y, newState) {
+    return false;
+}
+
+function explore(x, y) {
+    var newX;
+    var newY;
+    for (var i = -1; i < 2; i++) {
+        newX = x + i;
+        for (var j = -1; j < 2; j++) {
+            newY = y + j;
+            if (isInRange(newX, newY, SIZE)) {
+                changeState(x, y, VAL_VISIBLE);
             }
         }
     }
